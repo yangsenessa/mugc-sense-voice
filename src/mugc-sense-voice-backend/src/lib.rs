9c97b3f0ddbaa224959e24bd8ec9e74c670e515a
guangyong.yang <chunming.ygy@gmail.com>
2025-02-07 23:01:11 +1300

    bomb, fixed getrandom issue

--- src/mugc-sense-voice-backend/src/lib.rs
+++ src/mugc-sense-voice-backend/src/lib.rs
@@ -2,9 +2,19 @@ use ic_cdk_macros::update;
 use ndarray::{Array2, Array};
 use ndarray_rand::RandomExt;
 use rand::distributions::Distribution;
-// Import RandomExt for `random()`
-use rand::thread_rng; // Import thread_rng() to generate random numbers
+use rand::thread_rng;
 use rand_distr::Uniform;
+
+use core::num::NonZeroU32;
+use getrandom::{register_custom_getrandom, Error};
+
+const MY_CUSTOM_ERROR_CODE: u32 = Error::CUSTOM_START + 42;
+
+pub fn always_fail(_buf: &mut [u8]) -> Result<(), Error> {
+    let code = NonZeroU32::new(MY_CUSTOM_ERROR_CODE).unwrap();
+    Err(Error::from(code))
+}
+register_custom_getrandom!(always_fail);
 
 pub struct CnnModel {
     weights: Array2<f32>,
